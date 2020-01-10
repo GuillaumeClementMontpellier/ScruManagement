9dd7b22adb96e7cfb7121ae1f99b1e29155d7214
GUYON Valentin <valentin.guyon@IGTP5-PC16.isim.intra>
2020-01-10 10:49:15 +0100

    business ticket

--- business/system/Ticket.java
+++ business/system/Ticket.java
@@ -1,10 +1,56 @@
 package business.system;
 
+import java.text.SimpleDateFormat;  
+import java.util.Date; 
+
 public class Ticket extends Component {
 
-    //TODO :: add ticket to the database to code the constructor
-    public Ticket(){
+	private String titleTicket;
+    private String descriptionTicket;
+	private String statusTicket;
+	private Date creationDateTicket;
+	private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-    }
+	public Ticket(){
+	
+	}
 
+	public Ticket(int id, String title, String description, String status) {		
+		setTitleTicket(title);
+		setDescription(description);
+		setStatus(status);
+		setCreationDate(formatter.format(java.time.LocalDate.now()));
+	}
+
+	public String getTitleTicket() {
+		return this.titleTicket;
+	}
+
+	public void setTitleTicket(String title) {
+		this.titleTicket = title;
+	}
+	
+	public String getDescriptionTicket() {
+		return this.descriptionTicket;
+	}
+
+	public void setDescriptionTicket(String description) {
+		this.descriptionTicket = description;
+	}
+
+	public String getStatusTicket() {
+		return this.statusTicket;
+	}
+
+	public void setStatusTicket(String status) {
+		this.satusTicket = status;
+	}
+
+	public Date getCreationDateTicket() {
+		return this.creationDateTicket;
+	}
+
+	public Date setCreationDateTicket(Date date) {
+		this.creationDateTicket = date
+	}
 }
